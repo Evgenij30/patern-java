b628f9f7b91fc9ef8c6a1f369bad3966f70afd52
Viacheslav Shchupak <diequeeq@users.noreply.github.com>
2022-07-10 12:14:05 +0200

    Homework04 updated

---
+++ Patterns/Lesson04/Homework04/src/main/java/org/robotdreams/patterns/lesson4/homework4/homework04.java
@@ -0,0 +1,10 @@
+package org.robotdreams.patterns.lesson4.homework4;
+
+public class homework04 {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+
+        // TODO (Viacheslav): create an application
+    }
+}
