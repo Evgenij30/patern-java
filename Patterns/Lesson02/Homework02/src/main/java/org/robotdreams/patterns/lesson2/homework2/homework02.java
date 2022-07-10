f79b04713cf29047de70c243f7041ccd3371e3c2
Viacheslav Shchupak <diequeeq@users.noreply.github.com>
2022-07-10 10:57:04 +0200

    Task02Extract added

--- Patterns/Lesson02/Homework02/src/main/java/org/robotdreams/patterns/lesson2/homework2/homework02.java
+++ Patterns/Lesson02/Homework02/src/main/java/org/robotdreams/patterns/lesson2/homework2/homework02.java
@@ -5,27 +5,86 @@ public class homework02 {
     public static void main(String[] args) {
         System.out.println("Starting Homework 02");
 
-        new SafeDeleteExample(15).execute(42);
+        new Task01SafeDelete(10, 20).execute(42);
+        new Task02Extract(20).execute(11);
     }
 }
 
 /* Task 01: apply SafeDelete refactorings to not used code parts */
-class SafeDeleteExample {
+class Task01SafeDelete {
 
-    private final int input;
+    private final int usedInput;
+    private final int unusedInput;
 
-    public SafeDeleteExample(int input){
+    public Task01SafeDelete(int usedInput, int unusedInput){
 
-        this.input = input;
+        this.usedInput = usedInput;
+        this.unusedInput = unusedInput;
+
+        print("initialized", usedInput, 1);
     }
-    public void execute(int i) { print(i, 1);}
-    private void unusedMethod(int i) { print(i, 2);}
+    public void execute(int i) { print("executed", i, 2);}
 
-    private void print(int usedParam, int unusedParam) {
-        System.out.println("SafeDeleteExample received " + usedParam);
+    private void print(String operation, int usedParam, int unusedParam) {
+        System.out.println("SafeDeleteExample " + operation + " with " + usedInput);
     }
 
     // private void printTest(int usedParam, int unusedParam) {
     //     System.out.println("SafeDeleteExample received 0");
     // }
+}
+
+/* Task 02: apply extract refactorings as specified in code */
+class Task02Extract {
+
+    private final int first;
+
+    public Task02Extract(int first){
+        this.first = first;
+    }
+    public void execute(int second) {
+        print("add",
+                first,
+                second,
+                /* Extract to method named add */
+                first + second
+        );
+        print("add",
+                42, /* introduce constant named theAnswerToTheUltimateQuestion */
+                second,
+                /* Extract to method named add */
+                42 + second
+        );
+
+        print("subtract",
+                first,
+                second,
+                /* Extract to method named subtract */
+                first - second
+        );
+
+        print("subtract",
+                42, /* this should automatically be refactored by the "introduce constant" change */
+                second,
+                /* Extract to method named subtract */
+                42 - second
+        );
+
+        print("multiply",
+                first,
+                second,
+                /* Extract to method named multiply */
+                first * second
+        );
+        print("divide",
+                first,
+                second,
+                /* Extract to method named divide */
+                first / second
+        );
+    }
+
+    private void print(String name, int a, int b, int result) {
+        System.out.println(String.join("",name,"(",Integer.toString(a),",",Integer.toString(b),")=",Integer.toString(result)));
+    }
 }
